07cacc14df1cf81460ab557fe0c1f493d66ca36c
kubersharma24 <kubersharma562@gmail.com>
2026-03-28 15:50:37 +0530

    Armstrong - Added Scanner for Input

--- ArmStrong.java
+++ ArmStrong.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 class ArmStrong{
     public static void main(String[] args) {
         System.out.println("Enter Nummber :");
+        Scanner sc = new Scanner(System.in);
     }
 }
