a142c1b01752c04a6388f9ea312c7dd7096849fb
kubersharma24 <kubersharma562@gmail.com>
2026-04-07 22:48:36 +0530

    Changes resolved

--- Number_checker.java
+++ Number_checker.java
@@ -1,19 +1,17 @@
 import java.util.*;
 
 //checks the number is positive or negetive.
-
 class Number_checker{
     public static void main(String args[]){
         System.out.println("enter the number");
-        Scanner sc=new Scanner(System.in);
-        double n=sc.nextDouble();
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
 
-        if(n<0)
+        if (n<0)
             System.out.println("number "+n+" is negative");
-        else if(n>0)
+        else if (n>0)
             System.out.println("number "+n+" is a positive number");
         else if (n==0) 
             System.out.println(n+" is a neutral number");           
-
     }
 }
